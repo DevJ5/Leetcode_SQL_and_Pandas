0ef36f32417eb6c222ce4b4358a36ae82d2aeda3
DevJ5 <idevelopjs@gmail.com>
2024-11-25 10:08:39 +0100

    Select Cleanup 4

--- Select/Article_Views_I.py
+++ Select/Article_Views_I.py
@@ -2,13 +2,19 @@ import pandas as pd
 
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    subset = views[views["author_id"] == views["viewer_id"]]
-    subset = sorted(subset["author_id"].unique())
+    mask = views["author_id"] == views["viewer_id"]
+    filtered_df = views.loc[mask, ["author_id"]].rename(columns={"author_id": "id"})
+    distinct_ids = filtered_df.drop_duplicates()
+    result = distinct_ids.sort_values(by="id", ascending=True).reset_index(drop=True)
+    return result
 
-    return pd.DataFrame({"id": subset})
 
-
-# select distinct author_id as id
-# from Views
-# where author_id = viewer_id
-# order by id asc
+# SQL Variant
+"""SELECT DISTINCT
+    author_id AS id
+FROM
+    Views
+WHERE
+    author_id = viewer_id
+ORDER BY
+    id ASC"""
