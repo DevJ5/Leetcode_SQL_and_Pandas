cc3a1c10f4a122d37aac73fff7a2b81d100d7926
DevJ5 <idevelopjs@gmail.com>
2024-11-29 09:33:02 +0100

    Added Encodings Readme

--- Basic_Joins/Customer_Who_Visited_Without_Transactions.py
+++ Basic_Joins/Customer_Who_Visited_Without_Transactions.py
@@ -2,14 +2,25 @@ import pandas as pd
 
 
 def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
-    df = pd.merge(visits, transactions, on="visit_id", how="left")
-    df = df[df["transaction_id"].isnull()]
-    df = df.groupby("customer_id").size().reset_index(name="count_no_trans")
-    return df
+    merged_result = visits.merge(transactions, how="left", on="visit_id")
+    filtered_result = merged_result[merged_result["transaction_id"].isnull()]
+    result = (
+        filtered_result.groupby("customer_id").size().reset_index(name="count_no_trans")
+    )
+    return result
 
 
-# select customer_id, count(*) as count_no_trans
-# from Visits as V
-# left join Transactions as T on V.visit_id = T.visit_id
-# where T.transaction_id is null
-# group by customer_id
+# SQL Variant
+"""
+SELECT
+    customer_id, COUNT(*) AS count_no_trans 
+FROM
+    Visits
+LEFT JOIN
+    Transactions
+    ON Transactions.visit_id = Visits.visit_id
+WHERE
+    transaction_id IS NULL
+GROUP BY
+    customer_id
+"""
---
+++ encodings.md
@@ -0,0 +1,6 @@
+Summary of Encoding Methods:
+Encoding Method Best For Description
+Label Encoding Ordinal data (with a natural order) Converts categories into integer labels.
+One-Hot Encoding Nominal data (without order) Creates separate binary columns for each category.
+Binary Encoding High-cardinality data Converts categories into binary format.
+Target Encoding Categorical data with a target variable Replaces categories with the mean of the target.
