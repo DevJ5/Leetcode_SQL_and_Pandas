8233a988e107704a3546bcf4510c814ed1254164
DevJ5 <idevelopjs@gmail.com>
2024-11-30 11:11:53 +0100

    Basic Joins Cleanup 2

--- Basic_Joins/Rising_Temperature.py
+++ Basic_Joins/Rising_Temperature.py
@@ -2,23 +2,29 @@ import pandas as pd
 
 
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    weather["recordDate"] = pd.to_datetime(weather["recordDate"])
-    weather_shifted = weather.copy()
-    weather_shifted["recordDate"] = weather_shifted["recordDate"] + pd.to_timedelta(
-        1, unit="D"
+    pd.set_option("display.max_columns", None)
+    weather["yesterday"] = weather["recordDate"] - pd.Timedelta(days=1)
+    merged = weather.merge(
+        weather,
+        how="inner",
+        left_on="yesterday",
+        right_on="recordDate",
+        suffixes=("_x", "_y"),
     )
-    merged_df = pd.merge(
-        weather, weather_shifted, on="recordDate", suffixes=("_today", "_yesterday")
-    )
-    result = merged_df[
-        merged_df["temperature_today"] > merged_df["temperature_yesterday"]
-    ]
-    result = result[["id_today"]].rename(columns={"id_today": "Id"})
+    filtered = merged[merged["temperature_x"] > merged["temperature_y"]]
+    result = filtered[["id_x"]].rename(columns={"id_x": "id"})
     return result
 
 
-# SELECT t1.id
-# FROM Weather t1
-# JOIN Weather t2
-# ON t1.recordDate = t2.recordDate + INTERVAL 1 DAY
-# WHERE t1.temperature > t2.temperature
+# SQL Variant
+"""
+SELECT 
+    t1.id
+FROM
+    Weather t1
+JOIN
+    Weather t2
+    ON t1.recordDate = DATE_ADD(t2.recordDate, INTERVAL 1 DAY)
+WHERE
+    t1.temperature > t2.temperature
+"""
