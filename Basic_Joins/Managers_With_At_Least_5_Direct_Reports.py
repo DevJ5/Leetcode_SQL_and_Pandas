cdf78e85e1dfec36a4b6392bf1a14ebd2914c98c
DevJ5 <idevelopjs@gmail.com>
2025-06-05 15:59:06 +0200

    Basic Joins Cleanup 3

--- Basic_Joins/Managers_With_At_Least_5_Direct_Reports.py
+++ Basic_Joins/Managers_With_At_Least_5_Direct_Reports.py
@@ -1,8 +1,24 @@
-# select name
-# from Employee
-# where id in (
-#     select managerId
-#     from Employee
-#     group by managerId
-#     having count(*) >= 5
-# )
+import pandas as pd
+
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    merged = pd.merge(
+        employee,
+        employee["managerId"].value_counts(),
+        how="left",
+        left_on="id",
+        right_on="managerId",
+    )
+    filtered = merged[merged["count"] >= 5]
+    return filtered[["name"]]
+
+
+# SQL Variant
+"""
+SELECT NAME
+FROM   employee
+WHERE  id IN (SELECT managerid
+              FROM   employee
+              GROUP  BY managerid
+              HAVING Count(*) >= 5)
+"""
