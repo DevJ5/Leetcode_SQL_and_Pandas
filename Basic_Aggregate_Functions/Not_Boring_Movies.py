e85554a3d96890075a1997e93f26b37fbb1253ab
DevJ5 <idevelopjs@gmail.com>
2025-06-07 08:37:47 +0200

    Basic Aggregage Cleanup 1

--- Basic_Aggregate_Functions/Not_Boring_Movies.py
+++ Basic_Aggregate_Functions/Not_Boring_Movies.py
@@ -9,7 +9,15 @@ def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
     return cinema
 
 
-# select *
-# from Cinema
-# where LOWER(description) <> 'boring' and id % 2 = 1
-# order by rating desc
+# SQL Variant
+"""
+SELECT
+    *
+FROM
+    Cinema
+WHERE
+    LOWER(description) <> 'boring'
+    AND id % 2 = 1
+ORDER BY
+    rating DESC;
+"""
