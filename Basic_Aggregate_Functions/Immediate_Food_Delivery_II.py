52f3636e5aaf07b621d2c02f015ec5c00ca01355
DevJ5 <idevelopjs@gmail.com>
2025-07-03 09:49:51 +0200

    Basic Aggregate Cleanup  6

--- Basic_Aggregate_Functions/Immediate_Food_Delivery_II.py
+++ Basic_Aggregate_Functions/Immediate_Food_Delivery_II.py
@@ -1,8 +1,33 @@
-# select round(avg(if(order_date = customer_pref_delivery_date,1,0)) * 100,2) as immediate_percentage
-# from Delivery d
-# where (customer_id, order_date) in
-# (
-#     select customer_id, min(order_date) as first_order_date
-#     from Delivery
-#     group by customer_id
-# )
+import pandas as pd
+
+
+def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    sorted_df = delivery.sort_values(
+        ["customer_id", "order_date"], ascending=[True, True]
+    )
+    grouped = sorted_df.groupby(["customer_id"]).head(1)
+    immediate_percentage = len(
+        grouped[grouped["order_date"] == grouped["customer_pref_delivery_date"]]
+    ) / len(grouped)
+    return pd.DataFrame(
+        {"immediate_percentage": [round(immediate_percentage * 100, 2)]}
+    )
+
+
+# SQL Variant
+"""
+SELECT
+    ROUND(AVG(IF(order_date = customer_pref_delivery_date, 1, 0)) * 100, 2) AS immediate_percentage
+FROM
+    Delivery d
+WHERE
+    (customer_id, order_date) IN (
+        SELECT
+            customer_id,
+            MIN(order_date) AS first_order_date
+        FROM
+            Delivery
+        GROUP BY
+            customer_id
+    );
+"""
